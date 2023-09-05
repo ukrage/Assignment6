fd53bf4a7bfa45ad7a3c0ba5261b7b526d68c6ec
ukrage <slololrs.bsot810nm931@gmail.com>
2023-09-05 22:53:02 +0900

    HelloWorldを画面に出力

---
+++ src/main/java/com/hello/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.hello.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
